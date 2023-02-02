c4e3f603343f4bb4199830367836ebea2f97b99c
Rolando Maradiaga <rolandomaradiaga23@gmail.com>
2023-02-02 07:08:44 -0600

    Create and add new articles to the DB

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,15 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+
+  end
+
+
+  def create
+    @article = Article.new(params.require(:article).permit([:title, :description]))
+    @article.save
+    redirect_to @article
+    #render plain: @article.inspect this helps you finde what are you sending to db
+  end
 end
