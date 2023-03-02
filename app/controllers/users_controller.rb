7904a3a621877d1e8290d03e4d787a5421aeef80
Rolando Maradiaga <rolandomaradiaga23@gmail.com>
2023-03-02 07:11:08 -0600

    Validations added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
   def new
     @user = User.new
@@ -11,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user= User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the blog #{@user.username}, you have signed up"
       redirect_to articles_path
     else
@@ -19,16 +21,15 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page])
   end
 
   def edit
-    @user = User.find(params[:id])
+    
   end
 
   def update
-    @user = User.find(params[:id])
+    
     if @user.update(user_params)
       flash[:notice] = "Your account was updated"
       redirect_to @user
@@ -41,5 +42,8 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
